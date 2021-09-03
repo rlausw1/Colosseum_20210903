000d31f4dcec3e069b1651c398ba128060d5e332
김현지 <guswlhjk@naver.com>
2021-09-03 18:37:44 +0900

    서버응답 본문을 JSONObject에 거쳐서 로그 출력

--- app/src/main/java/com/nepplus/colosseum_20210903/utils/ServerUtil.kt
+++ app/src/main/java/com/nepplus/colosseum_20210903/utils/ServerUtil.kt
@@ -1,9 +1,10 @@
 package com.nepplus.colosseum_20210903.utils
 
 import android.app.DownloadManager
-import okhttp3.FormBody
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import android.util.Log
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
 class ServerUtil {
 
@@ -21,7 +22,7 @@ class ServerUtil {
 
 //        로그인 기능 실행 함수
 
-        fun postRequestSignIn( id : String, pw : String ) {
+        fun postRequestSignIn(id: String, pw: String) {
 
 //            1. 어디로(url) 갈것인가? HOST_URL + Endpoint
 //
@@ -33,7 +34,6 @@ class ServerUtil {
                 .add("email", id)
                 .add("password", pw)
                 .build()
-
 
 
 //                    3. 어떤 방식 접근? ㄱㄷ볃
@@ -49,16 +49,34 @@ class ServerUtil {
 //            요청을 한다 -> 클라이언트의 역할 ->앱이 클라이언트로 동작
 
             val client = OkHttpClient()
-            client.newCall(request)
 
 //만들어진 요청 호출 => 응답이 왔을떄 분석 / UI 반영
 //            호출을 하면 -> 응답 받아서 처리 ( 처리할 코드 등록)
+            client.newCall(request).enqueue(object  : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+
+//                    실패? 서버 연결 자체를 실패 응답 X
+//                    비번 틀려서 로그인 실패 : 응답은 돌아왔는데, 그 내용이 실패 (응답O)
+//                    인터넷 끊김, 서버 접속 불가 등. 실패 O
 
 
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+                    //                어떤 내용이든, 응답이 돌아온 경우 ( 로그인 성공, 실패 모두 응답)
+//                    응답에 포함된 데이터들중 -> 본문(body)을 보자
+
+                    val bodyString = response.body!!.string()
+
+//                    본문을 그냥 String으로 찍어보면 -> 한글이 깨져보임
+//                    JSONObject 형태로 변환해서 -> 다시 String으로 바꿔보면 한글이 보임
+                    val jsonObj = JSONObject(bodyString)
+
+                    Log.d("서버응답본문", jsonObj.toString())
 
 
-
-
+                }
 
 
             })
@@ -66,18 +84,11 @@ class ServerUtil {
 
 
 
-        }
-
-
-
-
-
-
-
-
-
 
     }
+
+
+}
 
 
 }
