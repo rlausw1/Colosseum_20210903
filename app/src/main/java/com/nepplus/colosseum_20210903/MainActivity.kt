bd316dcc78370a85f9979c63654f740102728b02
김현지 <guswlhjk@naver.com>
2021-09-03 15:52:03 +0900

    입력한 아이디/비번을 변수로 저장.  - 서버의 로그인 기능 활용 예정

--- app/src/main/java/com/nepplus/colosseum_20210903/MainActivity.kt
+++ app/src/main/java/com/nepplus/colosseum_20210903/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nepplus.colosseum_20210903
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.nepplus.colosseum_20210903.utils.ServerUtil
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +14,19 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        signInBtn.setOnClickListener {
+
+            //            입력한 아이디 비번 변수로 저장.
+
+            val inputId = emailEdt.text.toString()
+            val inputPw = passwordEdt.text.toString()
+
+//            서버에 이 데이터가 회원이 맞는지? 확인 요청.  => 로그인 시도.
+
+
+
+        }
 
 
 
