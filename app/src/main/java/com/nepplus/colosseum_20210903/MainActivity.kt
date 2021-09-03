48adb54ff7ebf07e5443be1647124daa808282a8
김현지 <guswlhjk@naver.com>
2021-09-03 19:11:04 +0900

    화면에 jsonObj 전달 -> 로그 찍어보기

--- app/src/main/java/com/nepplus/colosseum_20210903/MainActivity.kt
+++ app/src/main/java/com/nepplus/colosseum_20210903/MainActivity.kt
@@ -2,8 +2,10 @@ package com.nepplus.colosseum_20210903
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.nepplus.colosseum_20210903.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,8 +25,17 @@ class MainActivity : BaseActivity() {
             val inputPw = passwordEdt.text.toString()
 
 //            서버에 이 데이터가 회원이 맞는지? 확인 요청.  => 로그인 시도.
+//서버 로그인 시도 => 서버에 다녀오면 어떡할건지? 대응 가이드북 변수 첨부 ( 인터페이스 객체)
+            ServerUtil.postRequestSignIn(inputId, inputPw, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(jsonObj: JSONObject) {
 
-            ServerUtil.postRequestSignIn(inputId, inputPw)
+//                    서버가 보내준 jsonObj를 가지고 처리하는 코드 작성 영역
+
+Log.d("화면에서받은JSON", jsonObj.toString())
+
+                }
+
+            })
 
 
 
--- app/src/main/java/com/nepplus/colosseum_20210903/utils/ServerUtil.kt
+++ app/src/main/java/com/nepplus/colosseum_20210903/utils/ServerUtil.kt
@@ -9,6 +9,12 @@ import java.io.IOException
 class ServerUtil {
 
 //    단순 기능 수행 -> 서버에 요청을 날리고 -> 응답을 화면에 전달
+
+//    응답을 화면에 전달 : 나에게 발생한 이벤트를 -> 화면단에게 대신 해달라고 한다 (Interface 활용)
+    interface JsonResponseHandler {
+        fun onResponse( jsonObject: JSONObject )
+    }
+
 //    어떤 객체가 하던, 요청/응답 처리만 잘 되면 그만
 //    이런 함수를 만든다? => static 함수들로 활용. ServerUtil.기능( ) 코드 작성 가능
 
@@ -21,8 +27,9 @@ class ServerUtil {
         private val HOST_URL = "http://54.180.52.26"
 
 //        로그인 기능 실행 함수
+//        아이디/비번 전달 + 서버에 다녀오면 어떤 일을 할지? 인터페이스 객체 전달
 
-        fun postRequestSignIn(id: String, pw: String) {
+        fun postRequestSignIn(id: String, pw: String, handler : JsonResponseHandler) {
 
 //            1. 어디로(url) 갈것인가? HOST_URL + Endpoint
 //
@@ -81,7 +88,8 @@ class ServerUtil {
 //
 //                    Log.d("코드값", code.toString())
 
-
+//받아낸 jsonObj 를 통째로 -> 화면의 응답 처리 코드에  넘겨주자
+                    handler?.onResponse(jsonObj)
 
                 }
 
