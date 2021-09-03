2dabef6ac83c79f9bdbaf4a8a4f54b41c5594a62
김현지 <guswlhjk@naver.com>
2021-09-03 16:15:57 +0900

    ServerUtil에 로그인 수행 함수 작업중  - 호풀까지는 작업 완료   - 응답 받는 처리 아직 X

--- app/src/main/java/com/nepplus/colosseum_20210903/utils/ServerUtil.kt
+++ app/src/main/java/com/nepplus/colosseum_20210903/utils/ServerUtil.kt
@@ -1,5 +1,7 @@
 package com.nepplus.colosseum_20210903.utils
 
+import android.app.DownloadManager
+
 class ServerUtil {
 
 //    단순 기능 수행 -> 서버에 요청을 날리고 -> 응답을 화면에 전달
@@ -9,6 +11,50 @@ class ServerUtil {
     companion object {
 
 //        이 안에 만드는 변수 / 함수는 전부 static 처럼 동작함
+
+//        호스트 주소를 애초에 변수로 저장해두자 (가져다 쓰기 편하게 - ServerUtil안에서만)
+
+        private val HOST_URL = "http://54.180.52.26"
+
+//        로그인 기능 실행 함수
+
+        fun postRequestSignIn( id : String, pw : String ) {
+
+//            1. 어디로 갈것인가?
+//
+            val urlString = "${HOST_URL}/user"
+
+//            2. 어떤 데이터를 들고 갈것인가? 파라미터
+
+            val formData = FormBody.Builder()
+                .add("email", id)
+                .add("password", pw)
+                .build()
+
+
+
+
+//                    3. 어떤 방식 접근? ㄱㄷ볃
+//            모두 모아서 하나의 request 정보로 만들어주자
+
+            val request = Request.Builder()
+                .url(urlString)
+                .post(formData)
+                .build()
+
+//            만들어진 Request를 실제로 호출해야함
+//            요청을 한다 -> 클라이언트의 역할 ->앱이 클라이언트로 동작
+
+            val client = OkHttpClient()
+
+
+            client.newCall(request)
+
+
+
+
+        }
+
 
 
 
