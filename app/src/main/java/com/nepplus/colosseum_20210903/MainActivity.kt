ae19df861c711c1642effccf6438254a389cb3f2
김현지 <guswlhjk@naver.com>
2021-09-07 00:58:15 +0900

    어댑터 객체화, 리스트뷰와 연결  - 서버 통신이 더 오래 걸려서 목록에 반영이 안되는 경우에 대한 대처  - 비동기대처

--- app/src/main/java/com/nepplus/colosseum_20210903/MainActivity.kt
+++ app/src/main/java/com/nepplus/colosseum_20210903/MainActivity.kt
@@ -2,13 +2,16 @@ package com.nepplus.colosseum_20210903
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nepplus.colosseum_20210903.adapters.TopicAdapter
 import com.nepplus.colosseum_20210903.datas.TopicData
 import com.nepplus.colosseum_20210903.utils.ServerUtil
+import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
     val mTopicList = ArrayList<TopicData>()
+    lateinit var mTopicAdapter: TopicAdapter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +28,8 @@ class MainActivity : BaseActivity() {
     override fun setValues() {
 
         getMainDataFromServer()
+        mTopicAdapter = TopicAdapter(mContext, R.layout.topic_list_item, mTopicList)
+        topicListView.adapter = mTopicAdapter
     }
 
 //    서버에서, 메인화면에 보여줄 정보 받아오기
@@ -32,7 +37,6 @@ class MainActivity : BaseActivity() {
 
         ServerUtil.getRequestMainData(mContext, object : ServerUtil.JsonResponseHandler {
             override fun onResponse(jsonObj: JSONObject) {
-
 //                응답 - jsonObj 분석 (파싱) => 토론 주제들을 서버가 내려줌
 
                 val dataObj = jsonObj.getJSONObject("data")
@@ -54,9 +58,11 @@ class MainActivity : BaseActivity() {
 //                    mTopicList에 하나씩 추가
                     mTopicList.add(tempTopicData)
 
+               }
 
-
-
+//                목록의 변화 -> 리스트뷰인지 -> 새로고침 공지 -> 리스트뷰 변경 -> 백그라운드에서 UI 변경
+                runOnUiThread {
+                    mTopicAdapter.notifyDataSetChanged()
 
 
                 }
