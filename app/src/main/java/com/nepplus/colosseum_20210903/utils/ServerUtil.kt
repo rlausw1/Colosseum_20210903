d205033c768fac6ce90d735d5e1b6c222761adee
김현지 <guswlhjk@naver.com>
2021-09-06 18:13:50 +0900

    get 리퀘스트 함수 작성 완료

--- app/src/main/java/com/nepplus/colosseum_20210903/utils/ServerUtil.kt
+++ app/src/main/java/com/nepplus/colosseum_20210903/utils/ServerUtil.kt
@@ -153,6 +153,33 @@ class ServerUtil {
             val urlString = url.toString()
             Log.d("완성된URL", urlString)
 
+            val request = Request.Builder()
+                .url(urlString)
+                .get()
+                .build()
+
+            val client = OkHttpClient()
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+
+
+
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+                    val bodyString = response.body!!.string()
+                    val jsonObj = JSONObject(bodyString)
+                    Log.d("서버응답", jsonObj.toString())
+                    handler?.onResponse(jsonObj)
+
+
+                }
+
+            })
+
+
+
         }
 
 
