26943921cef2d9089ce8d07ebaeef003f55a9d76
김현지 <guswlhjk@naver.com>
2021-09-06 17:00:54 +0900

    가입 실패시에 대한 사유 띄우기

--- app/src/main/java/com/nepplus/colosseum_20210903/SignUpActivity.kt
+++ app/src/main/java/com/nepplus/colosseum_20210903/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.nepplus.colosseum_20210903
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.nepplus.colosseum_20210903.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import org.json.JSONObject
@@ -26,7 +27,25 @@ class SignUpActivity : BaseActivity() {
 //            2. 서버에 가입 요청 -> 응답에 따라 어떤 처리?
 
             ServerUtil.putRequestSignUp(inputEmail, inputPw, inputEmail, object : ServerUtil.JsonResponseHandler {
-                override fun onResponse(jsonObject: JSONObject) {
+                override fun onResponse(jsonObj: JSONObject) {
+
+//                    가입 성공(200) / 실패(200외의 값)
+//                    실패 : 이메일 양식 x, 중복된 이메일, 중복된 닉네임
+
+//                    성공일때 -> { } 내부 비워두자
+//                    실패시만 동작 -> message 에 담긴 가입 실패 사유를 갖고 -> 토스트로 출력
+
+                    val code = jsonObj.getInt("code")
+
+                    if(code == 200) {
+
+                    }
+                    else {
+                        val message = jsonObj.getString("message")
+                        runOnUiThread {
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
 
                 }
 
