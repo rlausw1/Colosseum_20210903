93ae67332a17f51ee9362701e51193c4eda287e1
김현지 <guswlhjk@naver.com>
2021-09-07 00:39:34 +0900

    서버의 토픽목록에서 -> topicData 목록으로 변환 작업중 -for 문 - topicData 보조 생성자 추가

--- app/src/main/java/com/nepplus/colosseum_20210903/MainActivity.kt
+++ app/src/main/java/com/nepplus/colosseum_20210903/MainActivity.kt
@@ -2,10 +2,15 @@ package com.nepplus.colosseum_20210903
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nepplus.colosseum_20210903.datas.TopicData
 import com.nepplus.colosseum_20210903.utils.ServerUtil
 import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
+
+    val mTopicList = ArrayList<TopicData>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,7 +31,28 @@ class MainActivity : BaseActivity() {
     fun getMainDataFromServer() {
 
         ServerUtil.getRequestMainData(mContext, object : ServerUtil.JsonResponseHandler {
-            override fun onResponse(jsonObject: JSONObject) {
+            override fun onResponse(jsonObj: JSONObject) {
+
+//                응답 - jsonObj 분석 (파싱) => 토론 주제들을 서버가 내려줌
+
+                val dataObj = jsonObj.getJSONObject("data")
+                val topicsArr = dataObj.getJSONArray("topics")
+
+//                서버가 내려주는 토론주제들 (JSONObjct 목록 ) =>  TopicData로 변환해서 ArrayList에 추가(반복문추가)
+
+                for ( i in 0 until topicsArr.length()) {
+//                    순서에 맞는 {} 를 통째로 받아내기 => 토론 주제 하나하나 담기
+                    val topicObj = topicsArr.getJSONObject(i)
+
+//                    topicData를 만들어서 -> 멤ㅂ변수들에 -> topicObj에서 파싱한 데이터를 대입
+
+                    val tempTopicData = TopicData()
+
+
+
+
+
+                }
 
             }
         })
--- app/src/main/java/com/nepplus/colosseum_20210903/datas/TopicData.kt
+++ app/src/main/java/com/nepplus/colosseum_20210903/datas/TopicData.kt
@@ -4,6 +4,9 @@ class TopicData(var id : Int,
                 var title : String,
                 var imageURL : String) {
 
+//    보조생성자 추가.
+    constructor() : this(0, "제목 없음", "")
+
 
 
 }
