8d0ff1dfb232194d876bb76edb5250a64882cf2d
김현지 <guswlhjk@naver.com>
2021-09-07 01:31:04 +0900

    자동로그인 여부 검토 -> 시작 화면을 다른곳으로 이동

--- app/src/main/java/com/nepplus/colosseum_20210903/SplashActivity.kt
+++ app/src/main/java/com/nepplus/colosseum_20210903/SplashActivity.kt
@@ -1,7 +1,11 @@
 package com.nepplus.colosseum_20210903
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.nepplus.colosseum_20210903.utils.ContextUtil
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +19,34 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed({
+
+//            1. 자동로그인 여부 판단 -> 상황에 따라 다른 화면으로 넘어가게
+//        다른 화면 : Intent의 목적지만 달라진다
+
+            val myIntent : Intent
+
+//        자동로그인 여부 : 사용자가 자동로그인 하겠다 + 저장된 토큰이 유효(들어있다)하다
+            if (ContextUtil.getAutoLogIn(mContext) && ContextUtil.getToken(mContext)!="" ) {
+
+//            둘다 만족 : 자동로그인 ok -> 메인화면으로 이동
+                myIntent = Intent(mContext, MainActivity::class.java)
+
+            }
+            else {
+//            하나라도 만족 안됨 : 자동 로그인 실패 -> 로그인 화면으로 이동
+                myIntent = Intent(mContext, SignInActivity::class.java)
+            }
+
+            startActivity(myIntent)
+            finish()
+
+
+        }, 2500)
+
+//
+
+
     }
 }
