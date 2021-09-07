a43390f5a5a69bb138952b46089ec3cd594203b8
김현지 <guswlhjk@naver.com>
2021-09-08 00:11:16 +0900

    BaseActivity에 커스텀 액션바 적용 코드 추가 => 메인화면에서 활용

--- app/src/main/java/com/nepplus/colosseum_20210903/BaseActivity.kt
+++ app/src/main/java/com/nepplus/colosseum_20210903/BaseActivity.kt
@@ -3,6 +3,8 @@ package com.nepplus.colosseum_20210903
 import android.content.Context
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity : AppCompatActivity() {
@@ -17,5 +19,22 @@ abstract class BaseActivity : AppCompatActivity() {
     abstract fun setupEvents()
     abstract fun setValues()
 
+//    액션바를 커스텀 액션바로 바꿔주는 함수
+
+    fun setCustomActionBar() {
+
+//        기본액션바를 불러오자
+        val defaultActionBar = supportActionBar!!
+
+//        커스텀모드로 변경 -> 우리가 만든  xml로 적용
+        defaultActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        defaultActionBar.setCustomView(R.layout.my_custom_action_bar)
+
+//        양 옆에 여백 제거 -> 모든 영역이 커스텀뷰
+        val myToolbar = defaultActionBar.customView.parent as Toolbar
+        myToolbar.setContentInsetsAbsolute(0,0)
+
+
+    }
 
 }
