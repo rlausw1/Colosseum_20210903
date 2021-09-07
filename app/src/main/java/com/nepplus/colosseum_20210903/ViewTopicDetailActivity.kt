f07aaca49901628cfafbd94385f757201671de2e
김현지 <guswlhjk@naver.com>
2021-09-07 11:08:30 +0900

    토론 상세정보 API 요청 -> 댓글 목록도 같이 불러옴  - mReplyList에 최종추가

--- app/src/main/java/com/nepplus/colosseum_20210903/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/nepplus/colosseum_20210903/ViewTopicDetailActivity.kt
@@ -3,6 +3,7 @@ package com.nepplus.colosseum_20210903
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
+import com.nepplus.colosseum_20210903.datas.ReplyData
 import com.nepplus.colosseum_20210903.datas.TopicData
 import com.nepplus.colosseum_20210903.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_view_topic_detail.*
@@ -11,6 +12,8 @@ import org.json.JSONObject
 class ViewTopicDetailActivity : BaseActivity() {
 
     lateinit var mTopicData: TopicData
+    val mReplyList = ArrayList<ReplyData>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,6 +49,23 @@ class ViewTopicDetailActivity : BaseActivity() {
 //            mTopicData를 새로 파싱한 데이터로 교체
 
             mTopicData = TopicData.getTopicDataFromJson(topicObj)
+
+//            추가 파싱, ui 반영
+            val repliesArr = topicObj.getJSONArray("replies")
+            for ( i in 0 until repliesArr.length()) {
+
+//                댓글 {] json -> replyData 파싱->mReplyList
+//                val replyObj = repliesArr.getJSONObject(i)
+//                val replyData = ReplyData.getReplyDataFromJson(replyObj)
+//                mReplyList.add(replyData)
+
+                mReplyList.add(ReplyData.getReplyDataFromJson(repliesArr.getJSONObject(i)))
+
+
+
+
+
+            }
 
 //            새로 받은 데이터로 ui 반영(득표수 등등)
             refreshTopicDataToUI()
