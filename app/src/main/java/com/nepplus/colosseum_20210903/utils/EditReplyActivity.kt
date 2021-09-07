48b4aa5e0f51a2423e4944cf4c4b481bf3db2dbf
김현지 <guswlhjk@naver.com>
2021-09-07 15:33:34 +0900

    댓글 작성 화면  추가

---
+++ app/src/main/java/com/nepplus/colosseum_20210903/utils/EditReplyActivity.kt
@@ -0,0 +1,22 @@
+package com.nepplus.colosseum_20210903.utils
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nepplus.colosseum_20210903.BaseActivity
+import com.nepplus.colosseum_20210903.R
+
+class EditReplyActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_reply)
+        setValues()
+        setupEvents()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+    }
+}
