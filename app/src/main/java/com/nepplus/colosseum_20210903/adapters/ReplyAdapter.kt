adcc52157407045dd1b63042ddc344778f2bb927
김현지 <guswlhjk@naver.com>
2021-09-08 01:23:35 +0900

    좋아요/싫어요 찍으면 액티비티에서 새로고침  - API 활용 / 태그 활용 등

--- app/src/main/java/com/nepplus/colosseum_20210903/adapters/ReplyAdapter.kt
+++ app/src/main/java/com/nepplus/colosseum_20210903/adapters/ReplyAdapter.kt
@@ -7,16 +7,21 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.nepplus.colosseum_20210903.R
+import com.nepplus.colosseum_20210903.ViewTopicDetailActivity
 import com.nepplus.colosseum_20210903.datas.ReplyData
 import com.nepplus.colosseum_20210903.datas.TopicData
+import com.nepplus.colosseum_20210903.utils.ServerUtil
+import org.json.JSONObject
 import java.text.SimpleDateFormat
 
 class ReplyAdapter(
     val mContext: Context,
     resId: Int,
-    val mList: List<ReplyData>) : ArrayAdapter<ReplyData>(mContext, resId, mList) {
+    val mList: List<ReplyData>
+) : ArrayAdapter<ReplyData>(mContext, resId, mList) {
 
 
     val mInflater = LayoutInflater.from(mContext)
@@ -51,9 +56,39 @@ class ReplyAdapter(
 
         createdAtTxt.text = data.getFormattedTimeAgo()
 
+        likeCountTxt.tag = true
+        hateCountTxt.tag = false
 
+//        해당 댓글에 좋아요/싫어요 찍었다 : 서버에 전송
 
+//        api : POST - topic_reply-like
+//        토큰값 / 댓글 id/ true/false (좋아요 싫어요) 파라미터
+//        도전과제 : 두개의 텍스트뷰가 눌리면 할일이 거의 동일
+//        차이점 - true 보내냐, flase를 보내냐
 
+        val ocl = object : View.OnClickListener {
+            override fun onClick(view: View?) {
+
+                val isLike = view!!.tag.toString().toBoolean()
+
+                ServerUtil.postRequestReplylikeOrHate(mContext, data.id, isLike, object
+                    : ServerUtil.JsonResponseHandler {
+                    override fun onResponse(jsonObject: JSONObject) {
+
+                        //어댑터 안에서 -> ViewTopicDetailActivity의 (mContext변수에 담겨있다!) 기능 실행.
+
+                        (mContext as ViewTopicDetailActivity).getTopicDetailDataFromServer()
+
+                    }
+                })
+            }
+        }
+
+//        tag 속성 이용, 하나의 코드에서 두개 대응
+//        추가설명 : 좋아요 / 싫어요 갯수 바로 변경되도록  (어댑터 -> 액티비티의 기능 실행)
+
+        likeCountTxt.setOnClickListener(ocl)
+        hateCountTxt.setOnClickListener(ocl)
 
 
 
